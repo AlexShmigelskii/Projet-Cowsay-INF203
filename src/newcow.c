00506efe31c93e31557315836b44cffe4c6e0a00
AlexShmigelskii <s5003626@gmail.com>
2025-04-18 13:36:28 +0200

    Enhance cow display function with customizable tail and leg lengths

--- src/newcow.c
+++ src/newcow.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void affiche_vache(char *eyes, char *tongue) {
+// Fonction d'affichage de la vache
+// Paramètres :
+//   eyes        : chaîne de 2 caractères pour les yeux
+//   tongue      : chaîne de un ou deux caractères pour la langue (peut être vide)
+//   tail_length : entier indiquant le nombre de caractères supplémentaires pour la queue
+//   legs_length : entier indiquant le nombre de lignes supplémentaires pour allonger les jambes
+void affiche_vache(char *eyes, char *tongue, int tail_length, int legs_length) {
+    int base_tail = 7; // nombre de '_' de base pour la queue
+
     printf("      \\    ^__^\n");
-    // On insère la chaîne eyes dans la partie correspondant aux yeux de la vache.
-    printf("       \\   (%s)\\_______\n", eyes);
+    // Ligne avec les yeux et la queue : on affiche le dessin de la tête suivi d'un nombre variable d'underscores.
+    printf("       \\   (%s)\\", eyes);
+    for (int i = 0; i < base_tail + tail_length; i++) {
+        printf("_");
+    }
+    printf("\n");
+
     printf("           (__)\\       )\\/\\\n");
-    switch (strlen(tongue)){
-        case 1: 
+    
+    // Affichage de la langue selon sa longueur
+    switch (strlen(tongue)) {
+        case 1:
             printf("            %s  ||----w |\n", tongue);
             break;
-        
-        case 2:    
+        case 2:
             printf("            %s ||----w |\n", tongue);
             break;
-        
         default:
             printf("               ||----w |\n");
             break;
     }
     printf("               ||     ||\n");
+    // Affichage des jambes : ligne de base, puis des lignes supplémentaires si legs_length > 0
+    for (int i = 0; i < legs_length; i++) {
+        printf("               ||     ||\n");
+    }
 }
 
 int main(int argc, char *argv[]) {
-    // Valeur par défaut pour les yeux : "oo" (deux caractères)
-    char eyes[3] = "oo";
-    char tongue[3] = "";
+    // Valeurs par défaut
+    char eyes[3] = "oo";      // yeux par défaut (2 caractères)
+    char tongue[3] = "";      // langue vide par défaut (affiche la ligne par défaut)
+    int tail_length = 0;      // queue par défaut sans allongement
+    int legs_length = 0;      // jambes par défaut (1 ligne seulement)
 
-    // Parcours des arguments pour traiter -e ou --eyes
+    // Analyse des arguments
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--eyes") == 0) {
-            if (i + 1 < argc) { // S'assurer qu'un argument suit l'option
+            if (i + 1 < argc) {
                 if (strlen(argv[i + 1]) == 2) {
                     strcpy(eyes, argv[i + 1]);
-                    i++; // On saute l'argument utilisé pour les yeux
+                    i++; // sauter l'argument traité
                 } else {
                     fprintf(stderr, "Erreur : l'argument pour -e doit être une chaîne de deux caractères.\n");
                     return 1;
@@ -43,13 +63,11 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
-        
-        if (strcmp(argv[i], "-T") == 0 || strcmp(argv[i], "--tongue") == 0) {
+        else if (strcmp(argv[i], "-T") == 0 || strcmp(argv[i], "--tongue") == 0) {
             if (i + 1 < argc) {
-                // Pour -T, on autorise une chaîne de un ou deux caractères
                 if (strlen(argv[i + 1]) <= 2) {
                     strcpy(tongue, argv[i + 1]);
-                    i++; // On saute l'argument utilisé pour la langue
+                    i++;
                 } else {
                     fprintf(stderr, "Erreur : l'argument pour -T doit être une chaîne de un ou deux caractères.\n");
                     return 1;
@@ -59,10 +77,28 @@ int main(int argc, char *argv[]) {
                 return 1;
             }
         }
+        else if (strcmp(argv[i], "--tail") == 0) {
+            if (i + 1 < argc) {
+                tail_length = atoi(argv[i + 1]);
+                i++;
+            } else {
+                fprintf(stderr, "Erreur : --tail requiert un argument numérique.\n");
+                return 1;
+            }
+        }
+        else if (strcmp(argv[i], "--legs") == 0) {
+            if (i + 1 < argc) {
+                legs_length = atoi(argv[i + 1]);
+                i++;
+            } else {
+                fprintf(stderr, "Erreur : --legs requiert un argument numérique.\n");
+                return 1;
+            }
+        }
+        // D'autres options pourraient être traitées ici.
     }
-
-    // Appel de la fonction affiche_vache
-    affiche_vache(eyes, tongue);
-
+    
+    // Appel de la fonction affiche_vache avec les paramètres personnalisés
+    affiche_vache(eyes, tongue, tail_length, legs_length);
     return 0;
 }
