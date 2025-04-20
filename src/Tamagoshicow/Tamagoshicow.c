a62edd8555e5f588e633440c4f452743bbcd3015
BakaosuikaG <gabriel.mella@laposte.net>
2025-04-20 18:57:40 +0200

    added Tamagoshicow.c, only include protos for now

---
+++ src/Tamagoshicow/Tamagoshicow.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define BYEBYELIFE 0
+#define LIFESUCKS 1
+#define LIFEROCKS 2
+
+
+void affiche_vache(int fitness){
+    //affiche une vache correspondant au niveau de fitness
+}
+
+int fitness_update(int fitness, int lunchfood){
+    //actualise la variable fitness en fonction de l'entree du joueur
+}
+
+int stock_update(int fitness, int lunchfood){
+    //actualise la variable stock en fonction de l'entree du joueur
+}
+
+int main(){
+    //
+    //TODO --> donner les args de main
+}
